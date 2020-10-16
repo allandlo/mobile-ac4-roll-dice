e69c26d00c930f99f0d2644b4f292b0c6be9282f
oould <oould@pop-os.localdomain>
2020-10-16 20:46:13 -0300

    App Mobile AC4

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  image : string;
+  images = [
+    '../../assets/dados/dado1.png',
+    '../../assets/dados/dado2.png',
+    '../../assets/dados/dado3.png',
+    '../../assets/dados/dado4.png',
+    '../../assets/dados/dado5.png',
+    '../../assets/dados/dado6.png'
+  ]
 
   constructor() {}
+
+  ngOnInit(){
+    this.image = this.images[0];
+  }
+
+  clicked(v){
+    let valor = Math.floor((Math.random() * 6));
+    this.image = this.images[valor]
+    v.value = valor+1
+  }
 
 }
